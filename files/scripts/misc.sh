b4347c51ddfcf96746a238e70968b787d1fa326b
Vorxiu <vorxiu@gmail.com>
2026-03-20 14:52:54 +0500

    install instructions for app2unit

--- files/scripts/misc.sh
+++ files/scripts/misc.sh
@@ -5,3 +5,10 @@ set -euo pipefail
 mkdir -p /nix
 
 rm /usr/share/applications/btop.desktop
+
+git clone https://github.com/Vladimir-csp/app2unit.git /app2unit
+cd /app2unit
+make
+sudo make install
+cd ..
+rm -rf app2unit
