000d7d350ab039dd18cbad1dda724081f97f2679
Vorxiu <vorxiu@gmail.com>
2026-03-20 17:59:48 +0500

    install app2unit and niri-settings

--- files/scripts/misc.sh
+++ files/scripts/misc.sh
@@ -6,9 +6,35 @@ mkdir -p /nix
 
 rm /usr/share/applications/btop.desktop
 
-git clone https://github.com/Vladimir-csp/app2unit.git /app2unit
-cd /app2unit
+# install app2unit
+git clone https://github.com/Vladimir-csp/app2unit.git /tmp/app2unit
+cd /tmp/app2unit
 make
 sudo make install
 cd ..
-rm -rf app2unit
+rm -rf /tmp/app2unit
+
+# install niri-settings
+git clone https://github.com/stefonarch/niri-settings.git /tmp/niri-settings
+cd /tmp
+echo "Installing to system..."
+# Install binary and .desktop file
+sudo cp -v niri-settings /usr/bin/niri-settings
+sudo chmod a+x /usr/bin/niri-settings
+sudo cp -v niri-settings.desktop /usr/share/applications/niri-settings.desktop
+
+# Install python files
+sudo mkdir -p /usr/lib/niri-settings/ui
+sudo cp -v niri_settings.py /usr/lib/niri-settings/
+sudo cp -av ui/*.py /usr/lib/niri-settings/ui
+
+# Install translations to standard XDG data directory
+sudo mkdir -p /usr/share/niri-settings/translations
+sudo cp -av translations/*.qm /usr/share/niri-settings/translations/
+
+# Icon
+sudo cp -v niri-settings.svg /usr/share/icons/hicolor/scalable/apps/niri-settings.svg
+
+echo ""
+echo "Installation finished."
+rm -rf /tmp/niri-settings
