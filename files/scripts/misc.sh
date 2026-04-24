95efe25f9f228b0fa12c17ab637b00e63753173e
vorxiu <vorxiu@gmail.com>
2026-04-24 18:31:09 +0500

    update

--- files/scripts/misc.sh
+++ files/scripts/misc.sh
@@ -53,6 +53,9 @@ rm -rf /tmp/sddm-noctalia
 # mkdir -p /etc/skel/.local/share/zinit/plugins
 # mkdir -p /etc/skel/.local/share/zinit
 
+mkdir -p /usr/share/icons
+wget -cO- https://github.com/phisch/phinger-cursors/releases/latest/download/phinger-cursors-variants.tar.bz2 | tar xfj - -C /usr/share/icons
+
 # git clone https://github.com/Aloxaf/fzf-tab.git /etc/skel/.local/share/zinit/plugins/Aloxaf---fzf-tab
 # git clone https://github.com/MichaelAquilina/zsh-auto-notify.git /etc/skel/.local/share/zinit/plugins/MichaelAquilina---zsh-auto-notify
 # git clone https://github.com/RobSis/zsh-completion-generator.git /etc/skel/.local/share/zinit/plugins/RobSis---zsh-completion-generator
