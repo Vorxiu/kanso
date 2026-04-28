5a36c341ac2264d2619c1163016447a20da8eced
vorxiu <vorxiu@gmail.com>
2026-04-28 10:22:44 +0500

    uncomment hook-post-rootfs in build-iso workflow and update configure-iso.sh script

--- files/scripts/configure-iso.sh
+++ files/scripts/configure-iso.sh
@@ -5,16 +5,16 @@ set -eoux pipefail
 dnf remove -y google-noto-fonts-all ublue-brew || true
 
 # Configure GNOME defaults for live session
-tee /usr/share/glib-2.0/schemas/zz2-org.gnome.shell.gschema.override <<EOF
-[org.gnome.shell]
-favorite-apps = ['anaconda.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop']
-EOF
+# tee /usr/share/glib-2.0/schemas/zz2-org.gnome.shell.gschema.override <<EOF
+#[org.gnome.shell]
+# favorite-apps = ['anaconda.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop']
+# EOF
 
-glib-compile-schemas /usr/share/glib-2.0/schemas
+# glib-compile-schemas /usr/share/glib-2.0/schemas
 
 # Disable services not needed in live environment
 systemctl disable rpm-ostree-countme.service
-systemctl disable tailscaled.service
+# systemctl disable tailscaled.service
 systemctl disable bootloader-update.service
 
 # Install Anaconda for installation
