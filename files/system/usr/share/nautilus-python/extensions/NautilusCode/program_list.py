00828ea7e780d19fd8461a7b12f71ea09a0f676d
vorxiu <vorxiu@gmail.com>
2026-05-01 10:51:00 +0500

    cleanup

---
+++ files/system/usr/share/nautilus-python/extensions/NautilusCode/program_list.py
@@ -0,0 +1,149 @@
+from gettext import gettext as _
+
+from .types import ProgramList, Program, Native, Flatpak
+
+progs = ProgramList()
+
+progs += Program('code-oss', _('Code-OSS'),
+                 Native('code-oss'),
+                 Flatpak('com.visualstudio.code-os'),
+                 supports_files=True)
+
+progs += Program('vscode', _('VSCode'),
+                 Flatpak('com.visualstudio.code'),
+                 supports_files=True)
+
+# Code-OSS also has a binary named 'code'. If Code-OSS is installed, the
+# command 'code' refers to Code-OSS instead of Microsoft VSCode. So, in
+# that case, we shouldn't show a menu entry for Microsft 'VSCode'.
+if not progs['code-oss']['Native'].is_installed:
+    progs['vscode'] += Native('code')
+
+progs += Program('code_insiders', _('VSCode (Insiders)'),
+                 Native('code-insiders'),
+                 Flatpak('com.visualstudio.code.insiders'),
+                 supports_files=True)
+
+progs += Program('vscodium', _('VSCodium'),
+                 Native('vscodium', 'codium'),
+                 Flatpak('com.vscodium.codium'),
+                 supports_files=True)
+
+progs += Program('codium_insiders', _('VSCodium (Insiders)'),
+                 Native('vscodium-insiders', 'codium-insiders'),
+                 Flatpak('com.vscodium.codium-insiders'),
+                 supports_files=True)
+
+progs += Program('gnome-builder', _('Builder'),
+                 Flatpak('org.gnome.Builder'),
+                 Native('gnome-builder'),
+                 arguments=['--project'])
+
+progs += Program('sublime', _("Sublime"),
+                 Flatpak("com.sublimetext.three"),
+                 Native("subl"))
+
+progs += Program('android-studio', _('Android Studio'),
+                 Native('studio'),
+                 Flatpak('com.google.AndroidStudio'))
+
+progs += Program('aqua', _('Aqua'),
+                 Native('aqua'))
+
+progs += Program('clion', _('CLion'),
+                 Native('clion'),
+                 Flatpak('com.jetbrains.CLion'),)
+
+progs += Program('clion-eap', _('CLion (EAP)'),
+                 Native('clion-eap'))
+
+progs += Program('cursor', _('Cursor'),
+                 Native('cursor'))
+
+progs += Program('datagrip', _('DataGrip'),
+                 Native('datagrip'),
+                 Flatpak('com.jetbrains.DataGrip'))
+
+progs += Program('datagrip-eap', _('DataGrip (EAP)'),
+                 Native('datagrip-eap'))
+
+progs += Program('dataspell', _('DataSpell'),
+                 Native('dataspell'))
+
+progs += Program('dataspell-eap', _('DataSpell (EAP)'),
+                 Native('dataspell-eap'))
+
+progs += Program('fleet', _('Fleet'),
+                 Native('fleet'))
+
+progs += Program('goland', _('GoLand'),
+                 Native('goland'),
+                 Flatpak('com.jetbrains.GoLand'))
+
+progs += Program('goland-eap', _('GoLand (EAP)'),
+                 Native('goland-eap'))
+
+# IntelliJ IDEA has multiple editions that default to the same command but
+# prioritise the highest-paying edition. Therefore the native version has been
+# treated as a separate program due to the uncertainty around edition.
+progs += Program('idea', _('IntelliJ IDEA'),
+                 Native('idea'))
+
+progs += Program('idea-eap', _('IntelliJ IDEA (EAP)'),
+                 Native('idea-eap'))
+
+progs += Program('idea-community', _('IntelliJ IDEA Community'),
+                 Flatpak('com.jetbrains.IntelliJ-IDEA-Community'))
+
+progs += Program('idea-professional', _('IntelliJ IDEA Ultimate'),
+                 Flatpak('com.jetbrains.IntelliJ-IDEA-Ultimate'))
+
+progs += Program('mps', _('MPS'),
+                 Native('mps'))
+
+progs += Program('phpstorm', _("PhpStorm"),
+                 Flatpak("com.jetbrains.PhpStorm"),
+                 Native("phpstorm"))
+
+progs += Program('phpstorm-eap', _("PhpStorm (EAP)"),
+                 Native("phpstorm-eap"))
+
+# PyCharm has multiple editions that default to the same command but
+# prioritise the highest-paying edition. Therefore the native version has been
+# treated as a separate program due to the uncertainty around edition.
+progs += Program('pycharm', _('PyCharm'),
+		 Native('pycharm'))
+
+progs += Program('pycharm-eap', _('PyCharm (EAP)'),
+		 Native('pycharm-eap'))
+
+progs += Program('pycharm-professional', _('PyCharm Professional'),
+		 Flatpak('com.jetbrains.PyCharm-Professional'))
+
+progs += Program('pycharm-community', _('PyCharm Community'),
+		 Flatpak('com.jetbrains.PyCharm-Community'))
+
+progs += Program('rider', _('Rider'),
+                 Native('rider'),
+                 Flatpak('com.jetbrains.Rider'))
+
+progs += Program('rubymine', _('RubyMine'),
+                 Native('rubymine'),
+                 Flatpak('com.jetbrains.RubyMine'))
+
+progs += Program('rubymine-eap', _('RubyMine (EAP)'),
+                 Native('rubymine-eap'))
+
+progs += Program('webstorm', _('WebStorm'),
+                 Native('webstorm'),
+                 Flatpak('com.jetbrains.WebStorm'))
+
+progs += Program('webstorm-eap', _('WebStorm (EAP)'),
+                 Native('webstorm-eap'))
+
+progs += Program('zed', _('Zed'),
+                 Native('zed', 'zedit', 'zeditor', 'zed-editor'),
+                 Flatpak('dev.zed.Zed'))
+
+progs += Program('zed-preview', _('Zed (Preview)'),
+                  Flatpak('dev.zed.Zed-Preview'))
---
+++ files/system/usr/share/nautilus-python/extensions/NautilusCode/types.py
@@ -0,0 +1,187 @@
+import os
+from gettext import gettext as _
+
+from gi.repository import Nautilus
+from gi.repository import GLib
+
+
+user_data_dir = GLib.get_user_data_dir()
+
+
+class NamedList (dict):
+    def __iter__ (self):
+        return self.values().__iter__()
+
+    @property
+    def names (self):
+        return self.keys()
+
+
+class Package:
+    run_command: tuple[str]
+    is_installed: bool
+    type_name = _('Unknown')
+
+    @property
+    def type_name_raw (self):
+        return self.__class__.__name__
+
+    def __str__ (self):
+        return f"{self.type_name}:\n  installed = {self.is_installed}"
+
+
+class Native (Package):
+    type_name = _('Native')
+    cmd_path = ''
+
+    def __init__ (self, *commands):
+      self.commands = commands
+      for cmd in commands:
+        if cmd_path := GLib.find_program_in_path(cmd):
+          self.cmd_path = cmd_path
+          break
+
+    @property
+    def run_command (self) -> tuple[str]:
+        '''The command that should be executed in order to
+           run a program using this type of package'''
+        return (self.cmd_path,)
+
+    @property
+    def is_installed (self):
+        return bool(self.cmd_path)
+
+    def __str__ (self):
+        lines = super().__str__().splitlines()
+        if self.cmd_path:
+          lines.insert(-1, f"  command = {os.path.basename(self.cmd_path)}")
+        elif self.commands:
+          lines.insert(-1, f"  command(s) = " + ', '.join(self.commands))
+        return  '\n'.join(lines)
+
+
+class Flatpak (Package):
+    type_name = _('Flatpak')
+    flatpak_path = GLib.find_program_in_path('flatpak') or ''
+    flatpak_bin_dirs = None
+
+    def __init__ (self, app_id: str):
+      self.app_id = app_id
+      self._calculate_bin_dirs()
+
+    def _calculate_bin_dirs(self):
+        if self.flatpak_bin_dirs is not None:
+            return
+
+        if self.__class__.flatpak_bin_dirs is not None:
+            self.flatpak_bin_dirs = self.__class__.flatpak_bin_dirs
+            return
+
+        self.flatpak_bin_dirs = self.__class__.flatpak_bin_dirs = []
+
+        postfix = '/flatpak/exports/bin'
+        for bin_dir in [
+          user_data_dir + postfix,
+          *os.environ.get('PATH', '').split(':'),
+          '/var/lib' + postfix
+          ]:
+          if (postfix in bin_dir
+          and bin_dir not in self.flatpak_bin_dirs):
+            self.flatpak_bin_dirs.append(bin_dir)
+
+    @property
+    def run_command (self) -> tuple[str]:
+        '''The command that should be executed in order to
+           run a program using this type of package'''
+        return (self.flatpak_path, 'run', self.app_id)
+
+    @property
+    def is_installed (self) -> bool:
+        if not self.flatpak_path:
+            return False
+
+        for bin_dir in self.flatpak_bin_dirs:
+            if os.path.exists (os.path.join (bin_dir, self.app_id)):
+                return True
+
+        return False
+
+    def __str__ (self):
+        lines = super().__str__().splitlines()
+        lines.insert(-1, f"  app_id = {self.app_id}")
+        return  '\n'.join(lines)
+
+
+class Program:
+    def __init__ (self, id:str, name:str, *packages, arguments:list[str]=None, supports_files=False):
+        self.id = id
+        self.name = name
+        self.arguments = arguments or []
+        self.supports_files = supports_files
+        self.packages = NamedList()
+        for pkg in packages:
+            self.add(pkg)
+
+    @property
+    def installed_packages (self):
+        pkgs = []
+        for pkg in self.packages:
+            if pkg.is_installed:
+                pkgs.append(pkg)
+        return pkgs
+
+    def add (self, pkg):
+        self.packages[pkg.type_name_raw] = pkg
+        return self
+
+    def __iadd__ (self, pkg):
+        return self.add(pkg)
+
+    def __getitem__ (self, type_name_raw):
+        return self.packages[type_name_raw]
+
+    def __str__ (self):
+        _str  =  'Program:'
+        _str += f'\n  Id: {self.id}'
+        _str += f'\n  Name: {self.name}'
+        if self.arguments:
+          _str += f'\n  Arguments: ' + ' '.join(repr(x) for x in self.arguments)
+        for pkg in self.packages:
+          for line in str(pkg).splitlines():
+            _str += '\n  ' + line
+        return _str
+
+
+class ProgramList (NamedList):
+
+    @staticmethod
+    def _activate_item (item, command: list[str]):
+        pid, *io = GLib.spawn_async(command)
+        GLib.spawn_close_pid(pid)
+
+    def get_menu_items (self, path, *, id_prefix='', is_file=False):
+        items = []
+
+        for program in self:
+            if is_file and not program.supports_files:
+                continue
+
+            installed_pkgs = program.installed_packages
+            include_type_name = True if len(installed_pkgs) > 1 else False
+            for pkg in installed_pkgs:
+
+                name = id_prefix + program.id
+                command = [*pkg.run_command, *program.arguments, path]
+                label = _('Open in %s') % program.name
+                if include_type_name:
+                    label += f' ({pkg.type_name})'
+
+                item = Nautilus.MenuItem.new(name, label)
+                item.connect('activate', self._activate_item, command)
+                items.append(item)
+
+        return items
+
+    def __iadd__ (self, value, /):
+        self[value.id] = value
+        return self
